0a0b9ea4a28871f4c49d71694ace1178ffe715df
Rino Nakagawara <nakagawara@reclick.co.jp>
2020-12-24 18:56:03 +0900

    feat: todo追加・削除機能を実装

---
+++ src/components/Body.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import List from "./List";
+import Form from "./Form";
+
+const Body = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (text) => {
+    setTodos([
+      ...todos,
+      {
+        id: Math.random().toString(32).substring(2),
+        body: text,
+        check: false,
+      },
+    ]);
+  };
+
+  console.log(todos);
+
+  const deleteTodo = (id) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
+
+  const putTodo = (id) => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.check = !todo.check;
+      }
+    });
+    setTodos(newTodos);
+  };
+
+  useEffect(() => {
+    setTodos(JSON.parse(localStorage.getItem("myTodos")) || []);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("myTodos", JSON.stringify(todos));
+  }, [todos]);
+
+  return (
+    <div className="task-body">
+      <div className="container">
+        <Form addTodo={addTodo} />
+        <List deleteTodo={deleteTodo} putTodo={putTodo} todos={todos} />
+      </div>
+    </div>
+  );
+};
+
+export default Body;
